27b61ec5e442ac35ba2b8160d5e72e2e40630b70
eyousefifar <eyousefifar@gmail.com>
2023-02-12 16:21:04 +0330

    fix csv format

--- app/usecases/rotateTable.ts
+++ app/usecases/rotateTable.ts
@@ -4,9 +4,9 @@ export function buildRotateTable() {
   return function rotateTable(id: string, table: number[]) {
     const madeTable = makeTable(table);
     if (madeTable.error) {
-      return `${id},[],false`;
+      return `${id},"[]",false`;
     }
     // JSON stringify can be faster
-    return `${id},${JSON.stringify(madeTable.get.shiftedTable())},true`;
+    return `${id},"${JSON.stringify(madeTable.get.shiftedTable())}",true`;
   };
 }
