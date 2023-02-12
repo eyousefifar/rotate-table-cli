4d5fbbb991a78dedad1370c32fe64c6a427b6ca7
eyousefifar <eyousefifar@gmail.com>
2023-02-12 16:22:07 +0330

    cleaner tests create makeTable once, reuse later. readable test data

--- app/__tests__/entities/table.ts
+++ app/__tests__/entities/table.ts
@@ -1,15 +1,14 @@
 import { buildMakeTable } from "../../entities/table";
 
 describe("table entity", () => {
+  const makeTable = buildMakeTable();
   it("should rotate odd ( 1 ) square tables", () => {
-    const makeTable = buildMakeTable();
     const table = [1];
     const madeTable = makeTable(table);
     expect(madeTable.error).toBeUndefined();
     expect(madeTable.get.shiftedTable()).toEqual([1]);
   });
   it("should rotate even ( 4 ) square tables", () => {
-    const makeTable = buildMakeTable();
     const table = [40, 20, 90, 10];
     const madeTable = makeTable(table);
     expect(madeTable.error).toBeUndefined();
@@ -17,24 +16,50 @@ describe("table entity", () => {
     expect(madeTable.get.shiftedTable()).toEqual([90, 40, 10, 20]);
   });
   it("should rotate even ( 16 ) square tables", () => {
-    const makeTable = buildMakeTable();
-    const table = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-    const madeTable = makeTable(table);
+    const table = [
+      [0, 1, 2, 3],
+      [4, 5, 6, 7],
+      [8, 9, 10, 11],
+      [12, 13, 14, 15],
+    ];
+    const rotatedTable = [
+      [4, 0, 1, 2],
+      [8, 9, 5, 3],
+      [12, 10, 6, 7],
+      [13, 14, 15, 11],
+    ];
+    const madeTable = makeTable(table.flat());
     expect(madeTable.error).toBeUndefined();
 
-    expect(madeTable.get.shiftedTable()).toEqual([
-      5, 1, 2, 3, 9, 10, 6, 4, 13, 11, 7, 8, 14, 15, 16, 12,
-    ]);
+    expect(madeTable.get.shiftedTable()).toEqual(rotatedTable.flat());
   });
   it("should rotate odd  ( 9 ) square tables", () => {
-    const makeTable = buildMakeTable();
     const table = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const madeTable = makeTable(table);
     expect(madeTable.error).toBeUndefined();
     expect(madeTable.get.shiftedTable()).toEqual([4, 1, 2, 7, 5, 3, 8, 9, 6]);
   });
+  it("should rotate odd  ( 25 ) square tables", () => {
+    const table = [
+      [1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10],
+      [11, 12, 13, 14, 15],
+      [16, 17, 18, 19, 20],
+      [21, 22, 23, 24, 25],
+    ];
+    const rotatedTable = [
+      [6, 1, 2, 3, 4],
+      [11, 12, 7, 8, 5],
+      [16, 17, 13, 9, 10],
+      [21, 18, 19, 14, 15],
+      [22, 23, 24, 25, 20],
+    ];
+    const madeTable = makeTable(table.flat());
+    expect(madeTable.error).toBeUndefined();
+    expect(madeTable.get.shiftedTable()).toEqual(rotatedTable.flat());
+  });
+
   it("should return error if table is not a perfect square", () => {
-    const makeTable = buildMakeTable();
     const table1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const table2 = [1, 2, 3];
     const madeTable1 = makeTable(table1);
